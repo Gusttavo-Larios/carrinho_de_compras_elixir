c6e50fce64caac35065eb07c695fa8eebf66a526
Gusttavo <eg9653641@gmail.com>
2023-03-16 21:56:47 -0300

    Fim dos tópicos Énum', 'Pattern Matching', 'Control Structures', 'Functions' e 'Pipe Operator'.

--- lib/carrinho_de_compras.ex
+++ lib/carrinho_de_compras.ex
@@ -7,46 +7,91 @@ defmodule CarrinhoDeCompras do
   #   %{key => value, key => value} ou %{atom: value, atom: value} -> mapa
   #     %{map | atom: value} -> atualizar chave já existente no mapa
   #     Map.put(mapa, :atom, value) -> inserir uma nova chave no mapa
+  # Enum
+  #   Módulo para se trabalhar com enumeráveis (coleções, com a exceção de tuplas)
 
-  # Adiciona o item no inicio da lista
-  def adicionar_item([item: item, lista_de_items: lista_de_items]) do
-    # [item | lista_de_items]
-    lista = lista_de_items ++ [item]
+  def adicionar_item(%{id: id, nome: nome, valor: valor}, lista_de_compras) do
+    lista = lista_de_compras ++ [%{id: id, nome: nome, valor: valor}]
     {:ok, lista}
   end
 
-  # def adicionar_item(_item, [1, 2, 3, 4, 5]) do
-  #   {:error, [mensagem: "Valores nulos não podem ser adicionandos á lista"]}
-  # end
-
-  # Remove os elementos em comum, e retorna apenas os que existem do lado esquerdo
-  def remover_item(item, lista_de_items) do
-    lista = lista_de_items -- [item]
+  def remover_item(lista_de_compras, id) do
+    lista = Enum.filter(lista_de_compras, fn item -> item.id != id end)
     {:ok, lista}
   end
 
-  # Juntando os elementos das duas listas (conjuntos)
   def misturar_listas(lista1, lista2) do
     lista = lista1 ++ lista2
     {:ok, lista}
   end
 
-  def obter_primerio_item_da_lista(lista_de_items) do
-    # [hd | _] = lista_de_items
-    lista = hd(lista_de_items)
+  def obter_primerio_item_da_lista(lista_de_compras) do
+    lista = hd(lista_de_compras)
     {:ok, lista}
   end
 
-  def obter_outros_elementos_lista(lista_de_items) do
-    # [_ | tl] = lista_de_items
-    lista = tl(lista_de_items)
+  def obter_outros_elementos_lista(lista_de_compras) do
+    lista = tl(lista_de_compras)
     {:ok, lista}
   end
 
-  def separar_lista(lista_de_items) do
-    {_, hd} = obter_primerio_item_da_lista(lista_de_items)
-    {_, tail} = obter_outros_elementos_lista(lista_de_items)
+  def separar_lista(lista_de_compras) do
+    {_, hd} = obter_primerio_item_da_lista(lista_de_compras)
+    {_, tail} = obter_outros_elementos_lista(lista_de_compras)
 
     %{:primeiro_item => hd, :resto_da_lista => tail}
+  end
+
+  # Pattern matching
+  def ordenar_por_preco(lista_de_compras, por_maior_preco: por_maior_preco) do
+    lista =
+      Enum.sort(lista_de_compras, fn primeiro_item, segundo_item ->
+        primeiro_item.valor > segundo_item.valor
+      end)
+
+    {:ok, lista}
+  end
+
+  def ordenar_por_preco(lista_de_compras) do
+    lista =
+      Enum.sort(lista_de_compras, fn primeiro_item, segundo_item ->
+        primeiro_item.valor < segundo_item.valor
+      end)
+
+    {:ok, lista}
+  end
+
+  def obter_total_da_lista(lista_de_compras) do
+    lista_de_valores = Enum.map(lista_de_compras, & &1.valor)
+
+    valor_total =
+      Enum.reduce(lista_de_valores, fn primeiro_item, segundo_item ->
+        primeiro_item + segundo_item
+      end)
+
+    quantidade_de_items = length(lista_de_compras)
+
+    {:ok,
+     %{
+       quantidade_de_items: quantidade_de_items,
+       valor_total: valor_total
+     }}
+  end
+
+  def realizar_compra(lista_de_compras, meio_de_pagamento) do
+    {_, %{quantidade_de_items: quantidade_de_items, valor_total: valor_total}} =
+      lista_de_compras |> obter_total_da_lista()
+
+    # if, unless, case, cond e with
+    cond do
+      valor_total < 500 && meio_de_pagamento != "Debito" ->
+        {:error, %{message: "Selecione o debito como meio de pagamento!"}}
+
+      valor_total >= 500 && valor_total <= 1000 && meio_de_pagamento != "Credito" ->
+        {:error, %{message: "Selecione o credito como meio de pagamento!"}}
+
+      true ->
+        {:ok, %{message: "Compra realizada com sucesso"}}
+    end
   end
 end
